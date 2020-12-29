44e6ffd3046c45e7ade75f244695af6292139611
ulzaman <ulzaman415@gmail.com>
2020-12-29 13:06:12 +0530

    Thiis is test for auto trigger

--- src/main/java/com/educian/base/Testbase.java
+++ src/main/java/com/educian/base/Testbase.java
@@ -29,7 +29,7 @@ import org.openqa.selenium.support.ui.Select;
 public class Testbase {
 
    public  static Properties prop;
-   public	static int attempts1 ;
+   public	static int attempts ;
 	WebElement uploadElement;
      String  actualResult;
 	String expectedResult;
